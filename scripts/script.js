00f4c5bb7992f120d50b4f275269c367bb79773f
karvaroz <vargasriosk@gmail.com>
2022-03-23 16:27:38 -0500

    Arreglando ultimos cambios en script y en css

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,30 @@
 async function fetchData() {
-  fetch("/data/data.json")
+  fetch("/data/mascotas.json")
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      let output = document.querySelector("container");
+      let output = document.querySelector("#container");
       data.mascotas.forEach((card) => {
-        let { id, name, especie, edad, description, raza, image } = data;
+        let { id, name, especie, edad, description, raza, image } = card;
         output.innerHTML += `
         <div class="card" id="${id}">
           <div class="card-header">
             <img src="${image}" alt="" />
           </div>
           <div class="card-body">
-            <h4>${name}</h4>
-            <h4>${especie}</h4>
-            <h4>${edad}</h4>
-            <h4>${raza}</h4>
-            <h4>${description}</h4>
+            <h2>${name}</h2>
+            <h4>Especie: ${especie}</h4>
+            <h4>Edad: ${edad}</h4>
+            <h4>Raza: ${raza}</h4>
+            <p>${description}</p>
+            <a href="#miModal">Adoptame!</a>
+            <div id="miModal" class="modal">
+              <div class="modal-contenido">
+                <a href="#">X</a>
+                <h2>Muchas gracias</h2>
+                <p>Adopta, no compres</p>
+              </div>  
+            </div>
           </div>
         </div>
         `;
